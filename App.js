00e8e6fc5c579f674c29f3c1e7c3bc404e267087
Prachi-0698 <prachikumari319@gmail.com>
2023-07-06 00:54:16 +0530

    Laying the Foundation

--- App.js
+++ App.js
@@ -2,38 +2,25 @@ import React from "react";
 import ReactDOM  from "react-dom/client";
 
 
-/**
- * <div id='parent'>
- * <div id='child'>
- * <h1>H1 tag</h1>
-    <h2>H2 tag</h2>
- * </div>
- * <div id='child'>
- * <h1>H1 tag</h1>
-    <h2>H2 tag</h2>
- * </div>
- * </div>
- */
+//JSX transpiled before it raeches to JS engine -- Parcel -- Babel
 
-// const heading = React.createElement(
-//     "h1",
-//     {id: "heading"},
-//     "Hello from React!"
-// )
+//JSX -> Babel transpiles it to React.createElement -> ReactElement - JS Object -> HTML Element (rendered)
 
-const parent = React.createElement('div', {id:'parent'},[
-    React.createElement('div', {id: 'child1'}, [
-        React.createElement('h1', {}, 'I am tag h1'),
-        React.createElement('h2', {}, 'I am tag h2')
-    ]),
-    React.createElement('div', {id: 'child2'}, [
-        React.createElement('h1', {}, 'I am tag h1'),
-        React.createElement('h2', {}, 'I am tag h2')
-    ])
-]);
+// const heading =    (<h1 className="heading">Namaste React from JSX!</h1>); 
 
-console.log(parent)  //its an object
+const Title = () =>(
+        <h1>This is the title!</h1>
+);
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent); //this render method is responsible to put that object as tag inside html
+// Component Composition
+const HeadingComponent = () => (
+    <div className="conatiner">
+        <Title />
+        <h2>Hello from Component</h2>
+    </div>
+)
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<HeadingComponent />);
