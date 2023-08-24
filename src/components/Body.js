4668d5bad7ddb0e7aad0e43565f25704b3b67cd9
Prachi-0698 <prachikumari319@gmail.com>
2023-08-24 12:33:37 +0530

    Using live API

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,27 @@
 import Restaurant from "./Restaurant";
 import resList from "../utils/mockData";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import resList from "../utils/mockData";
 
 const Body = () =>{
-    const [listOfRes, setListOfRes] = useState(resList);
+    const [listOfRes, setListOfRes] = useState([]);
+
+    useEffect(()=>{
+        fetchData();
+    }, [])
+
+    const fetchData = async () =>{
+      const data = await fetch(
+        "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6422668&lng=77.0845962&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      );
+
+      const json = await data.json();
+
+      console.log(json)
+
+      setListOfRes(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+    }
+
     return(
         <div className="body">
             <div className="filter">
@@ -24,7 +41,7 @@ const Body = () =>{
             </div>
             <div className="res-container">
                 {listOfRes.map((restaurant) =>(
-                    <Restaurant resData={restaurant} key={restaurant.data.id}/>
+                    <Restaurant resData={restaurant} key={restaurant?.card.card.id}/>
                 ))
                 }
                 
